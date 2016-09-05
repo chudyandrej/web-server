c77f33a5514fb5bef0361e06946013161cec0712
Andrej Chudy <xchudy03@stud.fit.vutbr.cz>
2016-09-05 16:55:03 +0200

    Init repo

---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+
+var middleware = {
+	requireAuthentification: function(req, res, next){
+		console.log('private route hit!');
+		next();
+
+	},
+	logger: function(req, res, next){
+		var date = Date().toString();
+		console.log('Request: '+ date + '  ' + req.method + ' ' + req.originalUrl);
+		next();
+	}
+};
+
+app.use(middleware.logger);
+
+
+app.get('/about', middleware.requireAuthentification, function(req, res){
+	res.send('About us!');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT, function (){
+	console.log('Server started on '+ PORT);
+});
